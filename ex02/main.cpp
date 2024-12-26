0bb5e1fc0862fccb0ca15e5849483fd001a07b35
Estelle van-anh beverley EGANA <estegana@paul-f4Br4s5.clusters.42paris.fr>
2024-12-26 20:56:37 +0100

    clean

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: estegana <estegana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 22:24:20 by estegana          #+#    #+#             */
-/*   Updated: 2024/12/23 22:37:47 by estegana         ###   ########.fr       */
+/*   Updated: 2024/12/26 19:42:30 by estegana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,14 @@ int main()
 	std::string *stringPTR = &v;
 	std::string &stringREF = v;
 
-	std::cout << "adresse de la memoire de la variable v: ";
-	std::cout << &v << std::endl;
-	std::cout << "adresse de la memoire de stringPTR : ";
-	std::cout << &stringPTR << std::endl;
-	//std::cout << "(adresse de la memoire de stringPTR right version: " << stringPTR << ")" << std::endl;
-	std::cout << "adresse de la memoire de stringREF : ";
-	std::cout << &stringREF << std::endl;
+	std::cout << "adresse de la memoire de la variable v: "<< &v << std::endl;
+	std::cout << "adresse de la memoire de stringPTR : " << &stringPTR << std::endl;
+	std::cout << "correction stringPTR : " << stringPTR << std::endl;
+	std::cout << "adresse de la memoire de stringREF : " << &stringREF << std::endl;
 	std::cout << std::endl;
-	std::cout << "valeur de la variable v : ";
-	std::cout << v << std::endl;
-	std::cout << "valeur de stringPTR : ";
-	std::cout << &stringPTR << std::endl;
-	//std::cout << "(adresse de la memoire de stringPTR right version: " << *stringPTR << ")" << std::endl;
-	std::cout << "valeur de stringREF : ";
-	std::cout << stringREF << std::endl;
+
+	std::cout << "valeur de la variable v : " << v << std::endl;
+	std::cout << "valeur de stringPTR : " << &stringPTR << std::endl;
+	std::cout << "correction valeur stringPTR : " << *stringPTR << std::endl;
+	std::cout << "valeur de stringREF : " << stringREF << std::endl;
 }
