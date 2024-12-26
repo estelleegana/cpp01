83ffccd1f2fd44330dc5dd7cceb47cfa34b00149
Estelle van-anh beverley EGANA <estegana@paul-f4Br4s5.clusters.42paris.fr>
2024-12-26 20:56:15 +0100

    init ex04

---
+++ ex04/main.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: estegana <estegana@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/26 20:35:29 by estegana          #+#    #+#             */
+/*   Updated: 2024/12/26 20:37:18 by estegana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+		std::cout << "enter the following parameters: ./losers filename string1 string2" << std::endl;
+	else
+	
+}
