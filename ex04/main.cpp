43b11d019b38b20c78a406370a3df0562b16b0c6
Estelle van-anh beverley EGANA <estegana@made-f0Ar3s1.clusters.42paris.fr>
2025-01-08 19:17:02 +0100

    copy success

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -19,13 +19,26 @@ int main(int argc, char **argv)
 		std::cout << "enter the following parameters: ./sed filename string1 string2" << std::endl;
 	else
 	{
+		//ouvrir le filename (argv1)
 		std::ifstream file;
-		file.open(argv[0]);
+		file.open(argv[1]);
+		//creer un nouveaufichiertxt
+		std::ofstream nouveau;
+		nouveau.open("exemple.replace");
+		//lire ligne par ligne et copier ds nouveaufichiertxt
 		std::string line;
 		getline(file, line);
-		std::cout << "getline de file : " << line << std::endl;
-		// std::ofstream replace;
-		// replace.open("exemple.replace");
-		// replace << line;
+		while (file)
+		{
+			nouveau << line;
+			nouveau << std::endl;
+			getline(file, line);
+		}
+		//remplacer s1 par s2 dans nouveaufichiertxt
+		std::string s1 = argv[2];
+		std::string s2 = argv[3];
+		//std::cout << "s1 et s2 : " << s1 << s2 << std::endl;
+		std::string verif;
+
 	}
 }
