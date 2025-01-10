71f870da40b7a99d27c056cf847ccaf64678b44d
Estelle van-anh beverley EGANA <estegana@paul-f4Br5s7.clusters.42paris.fr>
2025-01-10 17:00:40 +0100

    filename.replace

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -16,29 +16,39 @@
 int main(int argc, char **argv)
 {
 	if (argc != 4)
-		std::cout << "enter the following parameters: ./sed filename string1 string2" << std::endl;
+		std::cout << "enter the following parameters: ./sed <filename> string1 string2" << std::endl;
 	else
 	{
 		//ouvrir le filename (argv1)
 		std::ifstream file;
 		file.open(argv[1]);
-		//creer un nouveaufichiertxt
-		std::ofstream nouveau;
-		nouveau.open("exemple.replace");
-		//lire ligne par ligne et copier ds nouveaufichiertxt
+		if (!file)
+		{
+			std::cout << "file does not exist" << std::endl;
+			exit(1);
+		}
+		//creer un filename.replace
+		std::string nom1;
+		std::string nom2;
+		std::string nom_vf;
+		nom1 = argv[1];
+		nom2 = ".replace";
+		nom_vf = nom1 + nom2;
+		std::ofstream replace;
+		replace.open(nom_vf);
+		//lire ligne par ligne et copier ds filename.replace
 		std::string line;
 		getline(file, line);
 		while (file)
 		{
-			nouveau << line;
-			nouveau << std::endl;
+			replace << line;
+			replace << std::endl;
 			getline(file, line);
 		}
-		//remplacer s1 par s2 dans nouveaufichiertxt
+		//remplacer s1 par s2 dans filename.replace
 		std::string s1 = argv[2];
 		std::string s2 = argv[3];
 		//std::cout << "s1 et s2 : " << s1 << s2 << std::endl;
 		std::string verif;
-
 	}
 }
