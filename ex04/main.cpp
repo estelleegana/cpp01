5075ca1775e71abc5b48cf00f06b8b1d6b2ba7f0
Estelle van-anh beverley EGANA <estegana@paul-f5Ar1s5.clusters.42paris.fr>
2025-01-15 08:45:35 +0100

    erreur

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -14,18 +14,28 @@
 
 int main(int argc, char **argv)
 {
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+
 	if (argc != 4)
 		std::cout << "enter the following parameters: ./sed <filename> string1 string2" << std::endl;
 	else
 	{
-		//ouvr//<filename>, s1, s2ir le filename (argv1)
+		//ouvr le <filename>
 		std::ifstream file;
 		file.open(argv[1]);
+		//si le <filename> existe pas, quitter
 		if (!file)
 		{
 			std::cout << "file does not exist" << std::endl;
 			exit(1);
-		std::string s2 = argv[3];
+		}
+		//creer un filename.replace
+		std::string nom2;
+		std::string newname;
+		nom2 = ".replace";
+		newname = filename + nom2;
 		std::ofstream replace;
 		replace.open(newname);
 		//lire ligne par ligne et copier ds filename.replace
@@ -38,10 +48,6 @@ int main(int argc, char **argv)
 			getline(file, line);
 		}
 		//remplacer s1 par s2 dans filename.replace
-		std::string s1 = argv[2];
-		std::string s2 = argv[3];
-		// std::string wordtest = "non";
-		// std::cout << "compare : " << wordtest.compare(s1) << std::endl;
-		// std::cout << "compare : " << wordtest.compare(s2) << std::endl;
+		
 	}
 }
