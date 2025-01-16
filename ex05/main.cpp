d077e972dfd8a90f0adf7136dc6ebada055af1ac
Estelle van-anh beverley EGANA <estegana@paul-f5Ar1s1.clusters.42paris.fr>
2025-01-16 13:02:00 +0100

    ex05 done

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -1,12 +1,18 @@
 #include "Harl.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
-    //creation objet
-    Harl obj;
-    //declaration pointeur sur la fonction membre
-    void (Harl::*pointeur)(std::string) = &Harl::complain;
+    if (argc != 2)
+        std::cout << "./harl <level (debug, info, warning, error)>" << std::endl;
+    else
+    {
+        //creation objet
+        Harl obj;
+        //declaration pointeur sur la fonction membre
+        void (Harl::*pointeur)(std::string) = &Harl::complain;
 
-    //appel de la fonction membre
-    (obj.*pointeur)("debug");
+        //appel de la fonction membre
+        (obj.*pointeur)(argv[1]);
+    }    
+    
 }
